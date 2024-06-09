33ff886677832ab1debb26b91f574eae86b9379f
EdoardoSquillaci <161933930+EdoardoSquillaci@users.noreply.github.com>
2024-06-09 19:05:23 +0200

    Inizio Esercizio

---
+++ src/main/java/com/ferragnez/party/CheckGuest.java
@@ -0,0 +1,30 @@
+package com.ferragnez.party;
+
+
+import java.util.Scanner; //scanner
+
+public class CheckGuest {
+
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Scanner scanner = new Scanner (System.in); //scanner
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
